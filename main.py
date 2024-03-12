531ecc525cb668ab1a96f123af5b09d2c658283d
Robson Chagas <112735714+robsonchagas@users.noreply.github.com>
2024-03-12 21:58:55 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#Calcular valores
+valorA = float(input("Digite o valor de A: "))
+valorB = float(input("Digite o valor de B: "))
+multiplicado = valorA * valorB
+dividido = valorA / valorB
+adicao = valorA + valorB
+subtrai = valorA - valorB
+elevado = valorA ** valorB
+print("A multiplicado por B é: ", multiplicado)
+print("A dividido por B é: ", dividido)
+print("A mais B é: ", adicao, "e A menos B é: ", subtrai)
+print("A elevado a B é: ", elevado)
